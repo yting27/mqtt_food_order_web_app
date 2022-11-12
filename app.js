5b9536873064dc4ff4d2b908fed7acec2b7f0051
yting27 <ngyen713@gmail.com>
2022-11-12 21:53:53 +0800

    Added sample data and finish order details page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var fs = require('fs');
+var favicon = require('serve-favicon');
 var db_access = require('./database/access')
 
 var indexRouter = require('./routes/index');
@@ -14,6 +15,9 @@ var app = express();
 
 // Connect to DB
 db_access.connectToDB();
+
+
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
